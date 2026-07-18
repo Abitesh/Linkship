1d6c5282b6bbee442f5b33b8443466b4781d8922
Abitesh <abiteshpabbulate@gmail.com>
2026-07-18 22:53:30 +0530

    Debugging again

--- links/views.py
+++ links/views.py
@@ -28,7 +28,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
+from users.forms import UserRegisterForm
 
 def redirect_link(request, identifier: str):
     """
