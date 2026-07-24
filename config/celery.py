d742e41c70f29a6e980b0c45c98335f19f3b8ac6
Abitesh <abiteshpabbulate@gmail.com>
2026-07-24 16:32:01 +0530

    Celery

--- config/celery.py
+++ config/celery.py
@@ -3,7 +3,10 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "config.settings.local"
+)
 
 app = Celery('linksnip')
 
